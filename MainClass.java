ee19f1f87d9d090221e75f010ce63b8f1546f0cc
MauriellGarcia <mauriellcgp@gmail.com>
2023-03-14 18:08:26 -0600

    calculo mauriell

--- MainClass.java
+++ MainClass.java
@@ -16,6 +16,16 @@ static int areatriangulo(int a, int b){
  return (a*b)/2;
 }
 
+    static int arearectangulo(int b , int h){
+        return b*h ;
+    }
+    static int areapentagono(int p, int a){
+        return (p*a) / 2;
+    }
+    static int arearombo(int D,int d){
+        return (D*d)/2 ;
+    }
+
 public static void main(String[] args) {
   Scanner teclado = new Scanner(System.in);
   int opcion=0;
@@ -27,6 +37,9 @@ public static void main(String[] args) {
   System.out.println(" ======== 1. Cuadrado      ========= ");
   System.out.println(" ======== 2. Circulo       ========= ");
   System.out.println(" ======== 3. Triangulo     ========= ");
+  System.out.println(" ======== 4. Rectangulo    ========= ");
+  System.out.println(" ======== 5. Pentagono     ========= ");
+  System.out.println(" ======== 6. Rombo         ========= ");
   System.out.println(" ___________________________________ ");
   System.out.println(" Ingrese una opcion ");
   opcion = teclado.nextInt();
@@ -49,6 +62,28 @@ public static void main(String[] args) {
          
          System.out.println(" EL AREA DEL TRIANGULO ES :"+areatriangulo(base,altura));
   break;
+
+      case 4:
+          System.out.println(" Ingrese la BASE ");
+          int base_rec = teclado.nextInt();
+          System.out.println(" Ingrese la ALTURA ");
+          int altura_rec = teclado.nextInt();
+          System.out.println(" EL AREA DEL RECTANGULO ES :"+arearectangulo(base_rec,altura_rec));
+          break;
+      case 5:
+          System.out.println(" Ingrese el perimetro");
+          int perimetro = teclado.nextInt();
+          System.out.println("Ingrese el Apotema");
+          int apotema= teclado.nextInt();
+          System.out.println(" EL AREA DEL PENTAGONO ES :"+areapentagono(perimetro,apotema));
+          break;
+      case 6:
+          System.out.println("Ingrese el Diagonal Mayor");
+          int Diagonal_Ma=teclado.nextInt();
+          System.out.println("Ingrese el Diagonal Menor");
+          int Diagonal_Me=teclado.nextInt();
+          System.out.println(" EL AREA DEL ROMBO ES :"+arearombo(Diagonal_Ma,Diagonal_Me));
+          break;
 
   }
  
